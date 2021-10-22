2dd4a88814a8f9bc6cc57f40e67a7f15e7f2cf7e
Jonathan Gaull <jonbeesh@gmail.com>
2021-10-22 15:44:48 -0700

    Added GitHubUrlParser to the pulls endpoint
    - Removed hard coded owner and repo names
    - Broke out a function to generate error objects

--- src/pulls.js
+++ src/pulls.js
@@ -1,18 +1,32 @@
 'use strict';
 
 const got = require('got')
+
 const LinkParser = require('./link-parser')
+const GitHubUrlParser = require('./github-url-parser')
 
 const MAX_RESULTS_PER_PAGE = 3 //keeping this low so I don't blow up my API request limits
 
 module.exports.handler = async (event, context) => {
 
-  const repoUrl = event.queryStringParameters.repo_url
   const perPage = Math.min(event.queryStringParameters.per_page, MAX_RESULTS_PER_PAGE) || MAX_RESULTS_PER_PAGE
 
   //need to parse the repoUrl to obtain these
-  const owner = 'octocat'
-  const repo = 'hello-world'
+  const repoUrl = event.queryStringParameters.repo_url
+  if (!repoUrl) {
+    return objectForError('repo_url is a required paramater')
+  }
+
+  let owner
+  let repo
+  try {
+    const urlParser = new GitHubUrlParser(repoUrl)
+    owner = urlParser.owner
+    repo = urlParser.repo
+  }
+  catch (error) {
+    return objectForError(`Error parsing github url: ${error.message}`)
+  }
 
   const listPullsUrl = `https://api.github.com/repos/${owner}/${repo}/pulls?per_page=${perPage}&state=open`
 
@@ -24,17 +38,7 @@ module.exports.handler = async (event, context) => {
     pullsHeaders = response.headers
   }
   catch (error) {
-
-    return {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-      },
-      body: JSON.stringify({
-        message: 'Error loading pull requests from the GitHub API',
-        code: 1
-      }),
-    }
+    return objectForError(`Error loading pull requests from the GitHub API: ${error.message}`)
   }
 
   const commitsRequests = pullsBody.map(value => {
@@ -46,16 +50,7 @@ module.exports.handler = async (event, context) => {
     commitsResponses = await Promise.all(commitsRequests)
   }
   catch (error) {
-    return {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-      },
-      body: JSON.stringify({
-        message: 'Error loading commits from the GitHub API',
-        code: 1
-      }),
-    }
+    return objectForError(`Error loading commits from the GitHub API: ${error.message}`)
   }
   
 
@@ -81,5 +76,18 @@ module.exports.handler = async (event, context) => {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
     body: JSON.stringify(pulls),
+  }
+}
+
+function objectForError(message, code = 0) {
+  return {
+    statusCode: 500,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message,
+      code
+    }),
   }
 }
